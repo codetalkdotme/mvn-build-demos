997d10cbf40d91ff3f7910237e976d317fe74925
guobiao.xu <guobiao.xu@phicomm.com>
2018-05-22 17:29:34 +0800

    change

---
+++ test-parent/test-service1/src/main/java/com/phicomm/demo/DemoMain.java
@@ -0,0 +1,31 @@
+package com.phicomm.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by guobiao.xu on 2018/5/22.
+ */
+@SpringBootApplication
+@RestController
+public class DemoMain {
+
+    @Value("${config.env}")
+    private String env;
+
+    @Value("${config.serviceName}")
+    private String serviceName;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMain.class, args);
+    }
+
+    @RequestMapping("/")
+    public String hello() {
+        return String.format("Hello, this is %s of env %s!", serviceName, env);
+    }
+
+}
